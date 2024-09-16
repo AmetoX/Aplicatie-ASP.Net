5f071961f454e42d4a54a476b18865bcbe450308
AmetoX <67999655+AmetoX@users.noreply.github.com>
2024-09-16 19:06:36 +0300

    .

--- AplicatieASP/Components/DataBase/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AplicatieASP.Components.DataBase.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
-    }
-}
