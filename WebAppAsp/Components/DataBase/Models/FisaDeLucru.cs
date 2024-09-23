766ff50f9171d4dabf2042487e02f073f97fdea2
AmetoX <67999655+AmetoX@users.noreply.github.com>
2024-09-23 15:28:34 +0300

    database connection and populated

---
+++ WebAppAsp/Components/DataBase/Models/FisaDeLucru.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAppAsp.Components.DataBase.Models
+{
+    public class FisaDeLucru
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public string Date { get; set; }
+    }
+}
--- WebAppAsp/Program.cs
+++ WebAppAsp/Program.cs
@@ -1,7 +1,13 @@
 using WebAppAsp.Components;
+using WebAppAsp.Components.DataBase;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace WebAppAsp
 {
+    //Install-Package MySql.Data.EntityFrameworkCore
+    //Install-Package Pomelo.EntityFrameworkCore.MySql
+    //Install-Package Microsoft.EntityFrameworkCore.Tools
     public class Program
     {
         public static void Main(string[] args)
@@ -11,6 +17,14 @@ namespace WebAppAsp
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+
+            // Add DbContext to the services
+            builder.Services.AddDbContext<BookShelfContext>(options =>
+            options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+            ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")),
+            mysqlOptions =>
+                mysqlOptions.SchemaBehavior(MySqlSchemaBehavior.Ignore) // Ignore schema
+            ));
 
             var app = builder.Build();
 
